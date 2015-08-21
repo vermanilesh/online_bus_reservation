ee9672fcf872b87a4e595a47843a59bbeed31863
Nilesh <nverma@grepruby.com>
2015-08-21 11:35:15 +0530

    First Commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OnlineBusReservation::Application.config.secret_key_base = '31f5ebb29146dd9fad8caf7e20ea94c8333fb54d6a75c3c914447e202d8edc96a5dce4850ed64a2249a02204a4bc3e6a21a3ed613a064eb60972335562e2e2ab'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OnlineBusReservation::Application.config.session_store :cookie_store, key: '_Online_Bus_Reservation_session'
