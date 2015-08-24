33e15d6e7173f0e1e59a14542c12893596e9c352
Nilesh <nverma@grepruby.com>
2015-08-24 13:15:45 +0530

    commit- created a partial for header_nav_bar and changes in view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 OnlineBusReservation::Application.routes.draw do
   root 'welcome#index'
-  get "welcome/index"
+  #get "welcome/index"
+  
   devise_for :users
+  #delete "devise/sessions#destroy"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
