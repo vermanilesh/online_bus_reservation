3ed428c28717c253fcc8e0ec30513d275b3adf9a
Nilesh <nverma@grepruby.com>
2015-08-26 11:27:19 +0530

    commit- changes in dashboard view of Agency

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  
   protect_from_forgery with: :exception
+
   def after_sign_in_path_for(resource)
   	if resource_name.capitalize.to_s == "User"
   		users_dashboard_path
@@ -9,4 +11,5 @@ class ApplicationController < ActionController::Base
   		agencies_dashboard_path
   	end
   end
+
 end
