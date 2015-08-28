8311f9fa4dcc87988fc72827a757f581cb5d90ac
Nilesh <nverma@grepruby.com>
2015-08-28 19:56:38 +0530

    create Schedule Model added functionalities in Route model and route agency association

--- agency.rb
+++ agency.rb
@@ -1,6 +1,6 @@
 class Agency < ActiveRecord::Base
 	has_many :buses
-	has_many :routes, through: :buses 
+	has_many :routes
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
