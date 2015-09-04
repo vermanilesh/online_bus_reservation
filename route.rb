3caaa4ce5bb93bd3ae9a4a8f761251fe74ae5746
Nilesh <nverma@grepruby.com>
2015-09-04 10:20:17 +0530

    commit for using active_admin gem for administration part

--- route.rb
+++ route.rb
@@ -2,7 +2,8 @@ class Route < ActiveRecord::Base
 	validates :from_station, :to_station, :distance, presence: true
 	has_many :buses
   has_many :schedules
-	belongs_to :agencies
+	belongs_to :agency
+	
 
 	def route_name
 		"#{from_station}->#{to_station}"
