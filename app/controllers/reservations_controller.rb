8238b045bca4b999dbbddf9d2b935fe3f9528fa0
Nilesh <nverma@grepruby.com>
2015-09-16 20:43:23 +0530

    commit apply some jquery validation and changes from view level and back end

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,6 +1,7 @@
 class ReservationsController < ApplicationController
   before_action :authenticate_user!
   
+  
   def new
     @reservation = current_user.reservations.new
   end
@@ -9,16 +10,20 @@ class ReservationsController < ApplicationController
     @reservations = current_user.reservations
   end
 
+  def show
+    @reservation = current_user.reservations.find(params[:id])
+  end
+
   def create
     @reservation = current_user.reservations.new(reservation_params)
     @reservation.schedule_id = params[:schedule_id] 
     if @reservation.save
-      redirect_to root_path
+      redirect_to agency_schedule_reservation_path(params[:agency_id], params[:schedule_id], @reservation)
     end
   end
 
   private
     def reservation_params
-      params.require(:reservation).permit(:no_of_seats, :journy_date, :fare)
+      params.require(:reservation).permit(:no_of_seats, :fare, :journy_date, :id, :schedule_id)
     end
 end
