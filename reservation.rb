92adcb7f16cf385df267d41fc4185d71457e30f0
Nilesh <nverma@grepruby.com>
2015-09-24 17:05:52 +0530

    removed some unnecessary files

--- reservation.rb
+++ reservation.rb
@@ -8,4 +8,20 @@ class Reservation < ActiveRecord::Base
   def update_availabel_seats
     self.schedule.availabel = self.no_of_seats
   end
+
+  def get_route_name
+  	self.schedule.route.route_name
+  end
+
+  def get_registration_number
+  	self.schedule.bus.registration_number
+  end
+
+  def dep_time
+  	self.schedule.departure_time.strftime("%I:%M %p")
+  end
+
+  def arr_time
+  	self.schedule.arrival_time.strftime("%I:%M %p")
+  end
 end
